5aa4b584fb437c5820939d0648d7fbbd2afff32e
anirudh <anirudh07.anand@gmail.com>
2022-10-02 17:16:49 +0530

    rename file

--- Image Processing/Reading-Image-Video/read-image.py
+++
@@ -1,4 +0,0 @@
-##This python notebook shows how to read images using OpenCV's imread() function. 
-##imread() supports various file formats including, but not limited to, .bmp, .webp, .jpg, .jpeg, and .png.
-
-#Import necessary 
