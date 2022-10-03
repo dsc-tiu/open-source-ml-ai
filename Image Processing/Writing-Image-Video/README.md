3dbbda905acc774ca11102713a5c57256f29c306
anirudh <anirudh07.anand@gmail.com>
2022-10-03 21:12:03 +0530

    add opencv imwrite tutorial

--- Image Processing/Writing-Image-Video/README.md
+++ Image Processing/Writing-Image-Video/README.md
@@ -1,1 +1,16 @@
 ## Writing-Image-Video
+
+### Writing Images using OpenCV
+- OpenCV's imwrite() function can be used to write images to a specified Location
+
+    **Syntax:** 
+    cv2.imwrite(filename, image)
+
+    **Parameters:**
+    filename: A string representing the file name. The filename must include image format like .jpg, .png, etc.
+    image: It is the image that is to be saved.
+
+    **Return Value:** 
+    It returns true if image is saved successfully.
+
+    For more details, please refer to the associated Python script `write_image_opencv.py`
---
+++ Image Processing/Writing-Image-Video/write_image_opencv.py
@@ -0,0 +1,20 @@
+#This Python script shows how to write an image using OpenCV's imwrite() function.
+
+#Import the necessary libraries
+import os
+import cv2
+
+#Initialize path variables
+imput_path = "assets/python_logo.png"
+output_path = "assets/python_logo_save.png"
+
+#Display the image 
+#If you are not familar with reading images using OpenCV, please refer to the 'Reading-Image-Video' folder.
+img = cv2.imread("assets/python_logo.png")
+cv2.imshow("Image",img)
+
+#wait till user presses a key
+cv2.waitKey(0)
+
+#Write the image to the 'assets' folder
+cv2.imwrite(output_path,img)
